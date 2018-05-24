28365f9af5e896fbc1b8e6b7f4e69cc5e1c5f6ec
John De Jesus <Jd@dhcp-v104-220.mobile.uci.edu>
2018-05-23 19:11:47 -0700

    added

--- public/app.js
+++ public/app.js
@@ -26,9 +26,9 @@ $(document).on("click", "p", function() {
       // The title of the article
       $("#notes").append("<h2> Write some notes </h2>");
       // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
+      $("#notes").append("<input id='titleinput' name='title' placeholder='Title'>");
       // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      $("#notes").append("<textarea id='bodyinput' placeholder ='write something' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
